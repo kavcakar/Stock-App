e60d60b22cc97edf4ced0fd3c74342fbad0da50c
Emre kavcakar <kavcakaremre@gmail.com>
2023-07-31 19:05:02 -0400

    register

--- stock-app-starter/src/hooks/useAuthCall.jsx
+++ stock-app-starter/src/hooks/useAuthCall.jsx
@@ -1,6 +1,6 @@
 
   import axios from "axios"
-  import { fetchFail, fetchStart, loginSuccess} from "../features/authSlice"
+  import { fetchFail, fetchStart, loginSuccess, logoutSuccess, registerSuccess} from "../features/authSlice"
   import { useDispatch } from "react-redux"
   import { useNavigate } from "react-router-dom"
 
@@ -24,8 +24,45 @@ const useAuthCall = () => {
         console.log(error)
         }
     }
+   
+    const register = async (userInfo) => {
+      const BASE_URL = "https://13537.fullstack.clarusway.com/"
+      
+      dispatch(fetchStart())
+
+    try {
+      const { data } = await axios.post(`${BASE_URL}account/auth/register`, userInfo)
+      dispatch(registerSuccess(data))
+      navigate("/stock")
+      console.log(data)
+  } catch (error) {
+    dispatch(fetchFail)
+  console.log(error)
+  }
+}
+
+const logout = async () => {
+  const BASE_URL = "https://13537.fullstack.clarusway.com/"
   
-  return { login }
+  dispatch(fetchStart())
+
+try {
+  const { data } = await axios.post(`${BASE_URL}account/logout`)
+  dispatch(logoutSuccess(data))
+  navigate("/")
+  console.log(data)
+} catch (error) {
+dispatch(fetchFail)
+console.log(error)
+}
+}
+
+
+
+
+
+  
+  return { login, register, logout }
 }
 
 export default useAuthCall
--- stock-app-starter/src/pages/Register.jsx
+++ stock-app-starter/src/pages/Register.jsx
@@ -2,19 +2,18 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import LockIcon from "@mui/icons-material/Lock";
+import { Formik } from "formik";
 import image from "../assets/result.svg";
 import Grid from "@mui/material/Grid";
-import { Formik, Form } from 'formik';
-import { Link, useNavigate } from "react-router-dom";
-import { Box, TextField } from "@mui/material";
-import { useSelector } from "react-redux";
-import LoadingButton from "@mui/lab/LoadingButton";
+import RegisterForm, { registerSchema } from "./RegisterForm";
+import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
 
+import useAuthCall from "../hooks/useAuthCall";
 
 const Register = () => {
-  const navigate = useNavigate();
-  const { currentUser, error } = useSelector((state) => state.auth);
-  const { register } = useAuthCall()
+  const { register } = useAuthCall();
+
   return (
     <Container maxWidth="lg">
       <Grid
@@ -52,112 +51,26 @@ const Register = () => {
           >
             Register
           </Typography>
+
           <Formik
-          initialValues={{username:"",
-           first_name: "",
-            last_name: "",
-            email: "",
-             password: "" }}
-          // validationSchema={loginScheme}
-          onSubmit={(values, actions) => {
-            // login(values)
-          actions.resetForm()
-          actions.setSubmitting()
-    }}
-    >
-    {({values, handleChange, handleBlur, errors, touched}) => (
-      <Form>
-        <Box sx={{display:"flex", flexDirection: "column", gap: 2}}>
-        <TextField 
-          label="User Name"
-          name="username"
-          id="username"
-          type="username"
-          variant="outlined"
-          value={values.username || ""}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.username && Boolean(errors.username)}
-          helperText={touched.username && errors.username}
-          
-        />
-        
-        
-        <TextField 
-          label="First
-          Name"
-          name="first_name"
-          id="first_name"
-          type="text"
-          variant="outlined"
-          value={values?.firstname || ""}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.firstname && Boolean(errors.firstname)}
-          helperText={touched.firstname && errors.firstname}
-          
-        />
-        
-      <TextField 
-        label="Last Name"
-        name="last_name"
-        id="last_name"
-        type="text"
-        variant="outlined"
-        value={values?.lastname || ""}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        error={touched.lastname && Boolean(errors.lastname)}
-        helperText={touched.lastname && errors.lastname}
-        
-      />
-      <TextField 
-      label="email"
-      name="email"
-      id="email"
-      type="email"
-      variant="outlined"
-      value={values.email || ""}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.email && Boolean(errors.email)}
-      helperText={touched.email && errors.email}
-      
-    />
-    
-    
-    <TextField 
-      label="password"
-      name="password"
-      id="password"
-      type="password"
-      variant="outlined"
-      value={values?.password || ""}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={touched.password && Boolean(errors.password)}
-      helperText={touched.password && errors.password}
-      
-    />
-
-
-        <LoadingButton 
-        type="submit" 
-        variant="contained"
-        size = "large 
-        // loading={loading}
-        >
-        Submit
-        </LoadingButton>
-        </Box>
-      </Form>
-)}
-    </Formik>
-
-    <Box sx={{ textAlign: "center", mt: 2 }}>
-    <Link to="/">Do you have an account?</Link>
-  </Box>
-</Grid>
+            initialValues={{
+              username: "",
+              first_name: "",
+              last_name: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={registerSchema}
+            onSubmit={(values, actions) => {
+              register({ ...values, password: values.password });
+              actions.resetForm();
+              actions.setSubmitting(false);
+            }}
+            component={(props) => <RegisterForm {...props} />}
+          ></Formik>
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Link to="/">Do you have an account?</Link>
+          </Box>
         </Grid>
 
         <Grid item xs={0} sm={7} md={6}>
